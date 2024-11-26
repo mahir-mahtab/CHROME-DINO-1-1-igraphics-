25f5ed0e41792aea4169bf7939c5209243558a36
Mahir <alamgirmahmud37@gmail.com>
2024-11-27 01:46:53 +0600

    game over and reset screen done

--- iMain.cpp
+++ iMain.cpp
@@ -27,24 +27,47 @@ char obstacleImages[4][50] = {
 	"./obstacles/tile002.bmp",
 	"./obstacles/tile003.bmp",
 };
+char birdImg[4][50] = {
+	"./bird/tile000.bmp",
+	"./bird/tile001.bmp",
+	"./bird/tile002.bmp",
+	"./bird/tile003.bmp",
+};
+int count = 0;
+int birdCount = 0;
+int score = 0;
+int highScore = 0;
 int groundInitial = 80, groundBlock = 80, groundSpeed = 25, offset = 0, fps = 0;
 float dinoX = 200, dinoY = 105, dinoSpeed = 10;
 
 int x = 300, y = 300, r = 0;
 int startGameflage = 0;
-float jumpflag = 0, jumpupflag = 0, jumpdownflag = 0, height = 120, speed = 1.50, a = .006;
+float jumpflag = 0, jumpupflag = 0, jumpdownflag = 0, height = 120, speed = 1.55, a = .006;
 int dip = 0;
 struct Obstacle
 {
 	float x, y;
 	int index;
 };
+struct Bird
+{
+	float x, y;
+	int index;
+};
 
 vector<Obstacle> obstacles;
+vector<Bird> birds;
+void createBird();
+void updateBird();
+void birdPlot();
+
 void createObstackles();
 void updateObstackle();
 void obstacklePlot();
 void checkGameOver();
+void gameOver();
+void restartGame(int x, int y);
+void displayScore();
 /*
 	function iDraw() is called again and again by the system.
 */
@@ -91,13 +114,12 @@ void groundUpdate()
 		offset = 0;
 	}
 }
-int count = 0;
 
 void iDraw()
 {
 	// place your drawing codes here
 	iClear();
-	
+
 	if (startGameflage == 0)
 	{
 		iShowBMP(0, 0, "./output.bmp");
@@ -106,6 +128,7 @@ void iDraw()
 	{
 		game();
 	}
+	gameOver();
 
 	return;
 }
@@ -132,6 +155,10 @@ void iMouse(int button, int state, int mx, int my)
 		if (startGameflage == 0)
 		{
 			startGame(mx, my);
+		}
+		if (startGameflage == 2)
+		{
+			restartGame(mx, my);
 		}
 		x += 10;
 		y += 10;
@@ -183,7 +210,6 @@ void iSpecialKeyboard(unsigned char key)
 	}
 	// place your codes for other keys here
 }
-
 int main()
 {
 	// place your own initialization codes here.
@@ -191,6 +217,7 @@ int main()
 	iSetTimer(2000, standDino);
 	iSetTimer(17, iDraw);
 	iSetTimer(3000, createObstackles);
+	iSetTimer(5000, createBird);
 	iInitialize(800, 600, "demo");
 	return 0;
 }
@@ -208,8 +235,9 @@ void game()
 	dinoJump();
 	dipDino();
 	obstacklePlot();
+	birdPlot();
 	checkGameOver();
-
+	displayScore();
 
 	// last line border
 	border();
@@ -222,9 +250,14 @@ void standDino()
 void update()
 {
 	r++;
+
 	// standDino();
 	groundUpdate();
 	updateObstackle();
+	updateBird();
+	score += 1;
+
+	// Update high score if current score exceeds it
 }
 void dinoJump()
 {
@@ -274,7 +307,7 @@ void dipDino()
 
 		iShowBMP2(dinoX, dinoY, dipImg[(bool)(rand() % 6)], 0);
 		fps++;
-		if (fps == 60)
+		if (fps == 120)
 		{
 			dip = 0;
 			fps = 0;
@@ -298,26 +331,96 @@ void updateObstackle()
 	{
 		if (obstacles[i].x <= 0)
 		{
-			
-				obstacles.erase(obstacles.begin() + i);
-				i--;
-			
+
+			obstacles.erase(obstacles.begin() + i);
+			i--;
 		}
 		obstacles[i].x -= groundSpeed;
 	}
 }
-void obstacklePlot(){
-	for (int i = 0; i < obstacles.size(); i++){
+void obstacklePlot()
+{
+	for (int i = 0; i < obstacles.size(); i++)
+	{
 		iShowBMP2(obstacles[i].x, obstacles[i].y, obstacleImages[obstacles[i].index], 0);
 	}
 }
 
-void checkGameOver(){
-	for(int i=0;i<obstacles.size();i++){
-	if(dinoX-10<obstacles[i].x && dinoX+10>obstacles[i].x && (dinoY<(obstacles[i].y+20)))
+void checkGameOver()
+{
+	for (int i = 0; i < obstacles.size(); i++)
 	{
-		startGameflage=0;
-		printf("Game Over\n");
+		if (dinoX - 10 < obstacles[i].x && dinoX + 10 > obstacles[i].x && (dinoY < (obstacles[i].y + 20)))
+		{
+			startGameflage = 2;
+			printf("Game Over\n");
+		}
+	}
+	if (dip == 0)
+		for (int i = 0; i < birds.size(); i++)
+		{
+			if (dinoX + 10 > birds[i].x && dinoX < birds[i].x + 70 && (dinoY + 55 > (birds[i].y)) && (dinoY < (birds[i].y + 45)))
+			{
+				startGameflage = 2;
+			}
+		}
+}
+void createBird()
+{
+	Bird add;
+	add.x = 800;
+	add.y = birdCount % 2 == 0 ? 120 : 180;
+	add.index = rand() % 4;
+	birds.push_back(add);
+}
+void updateBird()
+{
+	for (int i = 0; i < birds.size(); i++)
+	{
+		if (birds[i].x <= 0)
+		{
+
+			birds.erase(birds.begin() + i);
+			i--;
+		}
+		birds[i].x -= (groundSpeed + 20);
+	}
+	birdCount++;
+}
+void birdPlot()
+{
+	for (int i = 0; i < birds.size(); i++)
+	{
+
+		iShowBMP2(birds[i].x, birds[i].y, birdImg[birdCount % 4], 16777215);
 	}
 }
+void gameOver()
+{
+	if (startGameflage != 2)
+	{
+		return;
+	}
+	else
+	{
+
+		iShowBMP2(0, 0, "./asset/gameover.bmp", -1);
+	}
+}
+void restartGame(int x, int y)
+{
+	if (x > 200 && x < 600 && y > 200 && y < 370)
+	{
+		startGameflage = 1, jumpdownflag = 0, jumpupflag = 0, jumpflag = 0, dip = 0, obstacles.clear(), birds.clear(), r = 0, birdCount = 0, dinoX = 200, dinoY = 105, fps = 0, speed = 1.55, a = .006;
+	}
+}
+void displayScore()
+{
+	iSetColor(255, 0, 255);											   // White color
+	iText(10, iScreenHeight - 20, "Score:", GLUT_BITMAP_HELVETICA_18); // Label
+	string scoreText;
+	scoreText = to_string(score);
+    char scorearr[scoreText.length() + 1];
+	strcpy(scorearr, scoreText.c_str());
+	iText(70, iScreenHeight - 20, scorearr, GLUT_BITMAP_HELVETICA_18);
 }
